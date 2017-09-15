abab5efc97270ff8929b882185884b165b9b20e0
Jack Zhang <jack.tiger.zhang@gmail.com>
2017-09-14 20:11:43 -0700

    completed hw1

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,12 +4,23 @@ pragma solidity ^0.4.13;
 contract greeter {
 	/* Add one variable to hold our greeting */
 	string greeting;
+	uint num_greetings;
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting;
+		num_greetings = 0;
 	}
 
-	function greet() constant returns (string)  {
+	function greet() returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		num_greetings += 1;
+		return greeting;
 	}
+
+	function num_greets() returns (uint) {
+	    /* returns the number of greetings so far */
+	    return num_greetings;
+	}
+
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,9 +2,21 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+
+	mapping(uint => uint) pastCalls;
+	event fibCall(uint position, uint result);
+
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		if (position<=2) {
+			return 1;
+		} else if (pastCalls[position] != 0) {
+			return pastCalls[position];
+		} else {
+			pastCalls[position] = calculate(position-1) + calculate(position-2);
+			return pastCalls[position];
+		}
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() {}
 }
